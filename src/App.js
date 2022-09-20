9ba248e52df6c931bd87272757d6ce835ebdbff2
AlonaPashko <105510952+AlonaPashko@users.noreply.github.com>
2022-09-20 15:29:16 +0200

    added a search engine

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
+import Dictionary from './Dictionary.js';
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
-       Hello from app!
-      </header>
+     </header>
+     <main>
+      <Dictionary />
+     </main>
+     <footer className="App-footer">
+      Coded by Alona Pashko
+     </footer>
+      </div>
     </div>
   );
 }
